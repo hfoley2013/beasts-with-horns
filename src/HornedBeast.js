754e1399102e129e60cae905f4a8d16eda7ba964
Harper Foley <harper.e.foley@gmail.com>
2022-11-08 01:12:44 +0000

    Cleans up formatting on HornedBeasts

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -7,9 +7,20 @@ class HornedBeast extends React.Component{
       <>
         <article>
           <div class="hornedBeastWrapper">
-            <h2 id={this.props.id} keyword={this.props.keyword}>{this.props.title}</h2>
-            <img src={this.props.image_url} title={this.props.title} alt={this.props.description}></img>
-            <p>{this.props.description}</p>
+            <h2
+              id={this.props.id} 
+              keyword={this.props.keyword}
+            >
+              {this.props.title}
+            </h2>
+            <img 
+              src={this.props.image_url} 
+              title={this.props.title} 
+              alt={this.props.description}
+            ></img>
+            <p>
+              {this.props.description}
+            </p>
           </div>
         </article>
       </>

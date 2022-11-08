51518f0a2b59105a9d760923c75894aa07dcc7d2
Harper Foley <harper.e.foley@gmail.com>
2022-11-08 01:10:29 +0000

    Adds data.json and forEach() loop to create an array of beasts

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+import data from './data.json';
+
+class Main extends React.Component{
+  render() {
+    let beastArr = [];
+    data.forEach((beast) => {
+      beastArr.push(
+        <HornedBeast 
+        _id={beast._id}
+        title={beast.title}
+        image_url={beast.image_url}
+        description={beast.description}
+        horns={beast.horns}
+        keyword={beast.keyword}
+      />
+      )
+    });
+    
+    return (
+      <>
+        <main>
+          {beastArr}
+        </main>
+      </>
+    );
+  }
+}
+
+export default Main;
